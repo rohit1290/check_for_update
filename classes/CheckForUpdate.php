ebc4d4f1675a53242e9502f24d50c3ec401fec15
Rohit Gupta <rohit_1290@yahoo.com>
2025-02-27 22:40:18 +0530

    update: cron frequency moved from daily to weekly

--- classes/CheckForUpdate.php
+++ classes/CheckForUpdate.php
@@ -5,8 +5,8 @@ class CheckForUpdate extends DefaultPluginBootstrap {
 
   public function init() {
     	// Update data on daily basis
-    	elgg_register_event_handler('cron', 'daily', 'update_check_for_update_table');
-    	elgg_register_event_handler('cron', 'daily', 'update_check_for_update_elgg');
+    	elgg_register_event_handler('cron', 'weekly', 'update_check_for_update_table');
+    	elgg_register_event_handler('cron', 'weekly', 'update_check_for_update_elgg');
 
     	elgg_extend_view("admin/dashboard", "check_for_update/alert_div", 1);
   }
